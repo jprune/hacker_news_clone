0a27eb15457949e666c59a67116c3938e0a7ffc9
Carlos Ferraz <carloslf@e.email>
2022-09-17 17:16:48 +0300

    prepping branch

---
+++ src/services/hackernews.js
@@ -0,0 +1,38 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// Define a service using a base URL and expected endpoints
+export const hackerNewsApi = createApi({
+  reducerPath: 'hackerNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://hn.algolia.com/api/v1/' }),
+  endpoints: (builder) => ({
+    getItems: builder.query({
+      query: (id) => `items/${id}`,
+    }),
+
+    getItemsFrontPage: builder.query({
+      query: () => 'search?tags=front_page&hitsPerPage=30',
+    }),
+
+    getUsers: builder.query({
+      query: (username) => `users/${username}`,
+    }),
+
+    search: builder.query({
+      query: (queryText) => `search?query=${queryText}`,
+    }),
+
+    getNewestItems: builder.query({
+      query: () => 'search_by_date?tags=story&hitsPerPage=30',
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const {
+  useGetItemsQuery,
+  useGetItemsFrontPageQuery,
+  useGetUsersQuery,
+  useSearchQuery,
+  useGetNewestItemsQuery,
+} = hackerNewsApi;
