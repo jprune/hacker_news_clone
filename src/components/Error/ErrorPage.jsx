72b8470ea6c776d58da2eec44f120238991b996e
Carlos Ferraz <carloslf@e.email>
2022-09-22 19:27:15 +0300

    Updating html to jsx

--- src/components/Error/ErrorPage.jsx
+++ src/components/Error/ErrorPage.jsx
@@ -1,40 +1,37 @@
 export default function ErrorPage({ data }) {
-  return (
+    return (
         <body>
         
-        <header className="header">
-            <nav className="nav container">
-                <a href="#" className="nav__logo">
-                    HACKERNEWS
-                </a>
-            </nav>
-        </header>
+            <header className="header">
+                <nav className="nav container">
+                    <a href="#" className="nav__logo">
+                        HACKERNEWS
+                    </a>
+                </nav>
+            </header>
 
-        <main className="main">
+            <main className="main">
             
-            <section className="home">
-                <div className="home__container container">
-                    <div className="home__data">
-                        <span className="home__subtitle">Error 404</span>
-                        <h1 className="home__title">Hey Buddy</h1>
-                        <p className="home__description">
-                            We can't seem to find the page <br> you are looking for.</br>
-                        </p>
-                        <a href="#" className="home__button">
-                            Head Home
-                        </a>
-                    </div>
+                <section className="home">
+                    <div className="home__container container">
+                        <div className="home__data">
+                            <span className="home__subtitle">Error 404</span>
+                            <h1 className="home__title">Hey Buddy</h1>
+                            <p className="home__description">
+                                We can't seem to find the page <br> you are looking for.</br>
+                            </p>
+                            <a href="#" className="home__button">
+                                Head Home
+                            </a>
+                        </div>
                     
-                    <div className="home__img">
-                        <img src="./images/ghost-img.png" alt="Ghost pic"></img>
-                        <div class="home__shadow"></div>
+                        <div className="home__img">
+                            <img src="./images/ghost-img.png" alt="Ghost pic"></img>
+                            <div class="home__shadow"></div>
+                        </div>
                     </div>
-                </div>
-            </section>
-        </main>
-
-        
-        <script src="/scrollreveal.min.js/scrollreveal.min.js"></script>
-
+                </section>
+            </main>
         </body>
-      
+    )
+}
