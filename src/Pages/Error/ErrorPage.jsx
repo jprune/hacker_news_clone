beeb03f96b856a132238db32f7a65a54b2b059be
Carlos Ferraz <carloslf@e.email>
2022-09-24 16:51:25 +0300

    Update fixes for past.jsx

--- src/Pages/Error/ErrorPage.jsx
+++ src/Pages/Error/ErrorPage.jsx
@@ -1,35 +1,33 @@
 import { Link } from 'react-router-dom';
-import image from '';
-import errorStyling from './styles.js';
 
 export default function ErrorPage() {
   return (
-  <>
-           <header className="header">
-                <nav className="nav container" >
-                    <h1 className="nav__logo">HACKERNEWS</h1>
-                </nav>
-            </header>
-            <main className="main">
-                <section className="home">
-                    <div className="home__container container">
-                        <div className="home__data">
-                            <span className="home__subtitle">Error 404</span>
-                            <h1 className="home__title">Hey Buddy</h1>
-                            <p className="home__description">
-                                We can't seem to find the page <br> you are looking for.</br>
-                            </p>
-                            <Link to="/" className="home__button">
-                                Back to safety
-                            </Link>
-                        </div>
-                        <div className="home__img">
-                          <img src={image} alt="Ghost error pic" />
-                            <div class="home__shadow"></div>
-                        </div>
-                    </div>
-                </section>
-            </main>
-        </>
-    )
+    <>
+      <header className="header">
+        <nav className="nav container">
+          <h1 className="nav__logo">HACKERNEWS</h1>
+        </nav>
+      </header>
+      <main className="main">
+        <section className="home">
+          <div className="home__container container">
+            <div className="home__data">
+              <span className="home__subtitle">Error 404</span>
+              <h1 className="home__title">Hey Buddy</h1>
+              <p className="home__description">
+                Unable to find the page <Br> you are looking for.</Br>
+              </p>
+              <Link to="/" className="home__button">
+                Back to safety
+              </Link>
+            </div>
+            <div className="home__img">
+              <img src={image} alt="Ghost error pic" />
+              <div className="home__shadow" />
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
--- src/Pages/Past/Past.jsx
+++ src/Pages/Past/Past.jsx
@@ -2,45 +2,58 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { Box, CircularProgress } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { Dayjs } from 'dayjs';
 import TextField from '@mui/material/TextField';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
-
 import { useGetPastItemsQuery } from '../../services/hackernews';
 
-
 export default function Past() {
-  const { data, error, isFetching } = useGetPastItemsQuery(value);
+  const [value, setValue] = useState(null);
 
-  const [value, setValue] = React.useState < Dayjs | null > (null);
-  
   useEffect(() => {
+    const { data, error, isFetching } = useGetPastItemsQuery(value);
 
     if (isFetching) {
-    return (
-      <Box display="flex" justifyContent="center" alignItems="center">
-        <CircularProgress className="text-slate-700 mt-24" size="8rem" />
-      </Box>
-    );
-  }
-  if (error) {
-    return (
-      //Error page HERE!!!?
-      <Box display="flex" justifyContent="center" alignItems="center">
-        <Link to="/src/components/Error/ErrorPage.jsx"></Link>
-      </Box>
-    );
-  }
+      return (
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <CircularProgress className="text-slate-700 mt-24" size="8rem" />
+        </Box>
+      );
+    }
+    if (error) {
+      return (
+      // Error page HERE!!!?
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <Link to="/error" />
+        </Box>
+      );
+    }
 
-    useGetPastItemsQuery();
+    return (
+      <ul className="px-[190px] py-10 text-slate-300 bg-slate-800 flex justify-start flex-wrap">
+        {data?.hits.map((item, i) => (
+          <div className="flex flex-wrap w-full h-20
+        border-solid border-2 px-4 py-2 my-2 rounded-xl border-slate-400
+        bg-slate-700 transition ease-in-out delay-40 hover:-translate-y-1 hover:scale-110 hover:bg-slate-900 duration-300
+        "
+          >
+            <li
+              key={i}
+              className="w-full font-bold text-slate-100"
+            >
+              {i}. {item.title} ({item.url})
+            </li>
+            <p className="text-sm">{item.points} by {item.author} {item.created_at} | hide | {item.num_comments} comments</p>
+          </div>
+        ))}
+      </ul>
+    );
   }, [value]);
 
   return (
-    <>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DatePicker
         label="Choose your date"
         value={value}
@@ -50,24 +63,5 @@ export default function Past() {
         renderInput={(params) => <TextField {...params} />}
       />
     </LocalizationProvider>
-
-     <ul className="px-[190px] py-10 text-slate-300 bg-slate-800 flex justify-start flex-wrap">
-      {data?.hits.map((item, i) => (
-        <div className="flex flex-wrap w-full h-20
-        border-solid border-2 px-4 py-2 my-2 rounded-xl border-slate-400
-        bg-slate-700 transition ease-in-out delay-40 hover:-translate-y-1 hover:scale-110 hover:bg-slate-900 duration-300
-        "
-        >
-          <li
-            key={i}
-            className="w-full font-bold text-slate-100"
-          >
-            {i - 1}. {item.title} ({item.url})
-          </li>
-          <p className="text-sm">{item.points} by {item.author} {item.created_at} | hide | {item.num_comments} comments</p>
-        </div>
-      ))}
-      </ul>
-  </>
-  )
+  );
 }
